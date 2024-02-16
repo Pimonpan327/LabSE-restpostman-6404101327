d9f9f42be60f1209872d7efe7175e66330868d8f
Pimonpan327 <Pimonpan01102002@gmail.com>
2024-02-16 10:57:09 +0700

    body-parser

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
 const app = express()
 
 app.get('/', (req, res) => {
